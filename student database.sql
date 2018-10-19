804bfef9366b5d1a96815caf51770238477d2327
lovekhatod <43640840+lovekhatod@users.noreply.github.com>
2018-10-19 17:58:46 +0530

    Add files via upload

---
+++ student database.sql
@@ -0,0 +1,5 @@
+insert into student values (1001,01,'love',100);
+insert into student values (1002,02,'piya',80);
+insert into student values (1003,03,'kangana',70);
+insert into student values (1004,04,apla,45);
+select * from student;
